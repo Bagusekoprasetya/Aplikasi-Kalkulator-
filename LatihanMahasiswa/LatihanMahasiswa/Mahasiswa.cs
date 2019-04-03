1a0f40862ef56051007fcc6c1482680a39c85684
Bagus eko <baguseko8814@gmail.com>
2019-04-03 13:27:58 +0700

    LatihanMahasiswa

---
+++ LatihanMahasiswa/LatihanMahasiswa/Mahasiswa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMahasiswa
+{
+    public class Mahasiswa
+    {
+        public Mahasiswa()
+        {
+
+        }
+
+        public Mahasiswa(string nim, string nama)
+        {
+            nim = nim;
+            nama = nama;
+        }
+
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+          
+    }
+}
